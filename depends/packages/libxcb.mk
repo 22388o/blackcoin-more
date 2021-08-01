d87ba08ed94bf28eda520a52ad7df3666b4ca879
Michel van Kessel <michelvankessel@protonmail.com>
2021-08-01 20:20:54 +0200

    cross compile issue solved

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=98d9ab05b636dd088603b64229dd1ab2d2cc02ab807892e107d674f9c
 $(package)_dependencies=xcb_proto libXau xproto
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static --disable-build-docs --without-doxygen --without-launchd
+$(package)_config_opts=--disable-static
 $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 # Because we pass -qt-xcb to Qt, it will compile in a set of xcb helper libraries and extensions,
 # so we skip building all of the extensions here.
@@ -27,8 +27,13 @@ define $(package)_preprocess_cmds
   sed "s/pthread-stubs//" -i configure
 endef
 
+# Don't install xcb headers to the default path in order to work around a qt
+# build issue: https://bugreports.qt.io/browse/QTBUG-34748
+# When using qt's internal libxcb, it may end up finding the real headers in
+# depends staging. Use a non-default path to avoid that.
+
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_autoconf) --includedir=$(host_prefix)/include/xcb-shared
 endef
 
 define $(package)_build_cmds
@@ -40,5 +45,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf share lib/*.la
+  rm -rf share/man share/doc
 endef
