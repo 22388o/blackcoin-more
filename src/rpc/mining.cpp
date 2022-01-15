7f22566f5e2755ddab454f3b98132bd495c07b29
Michel van Kessel <michelvankessel@protonmail.com>
2022-01-15 16:26:24 +0100

    trying to get the correct weight

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -270,9 +270,11 @@ UniValue getstakinginfo(const UniValue& params, bool fHelp)
     if (pwalletMain)
         nWeight = pwalletMain->GetStakeWeight();
 
-    uint64_t nNetworkWeight = 1.1429 * GetPoSKernelPS();
+    uint64_t nNetworkWeight = GetPoSKernelPS();
     bool staking = nLastCoinStakeSearchInterval && nWeight;
-    uint64_t nExpectedTime = staking ? 1.0455 * 64 * nNetworkWeight / nWeight : 0;
+    const Consensus::Params& consensusParams = Params().GetConsensus();
+    int64_t nTargetSpacing = consensusParams.nTargetSpacing;
+    uint64_t nExpectedTime = staking ? (nTargetSpacing * nNetworkWeight / nWeight) : 0;
 
     UniValue obj(UniValue::VOBJ);
 
