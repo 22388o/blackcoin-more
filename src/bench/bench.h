daab51bacaeab1d3a40d5aa266f56c8f1fb112b3
Michel van Kessel <michelvankessel@protonmail.com>
2022-01-27 21:06:42 +0100

    Revert "update bench"
    This reverts commit e0ed4ec574e135d2c46420cb90f0be32739cf4bf.

--- src/bench/bench.h
+++ src/bench/bench.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <string>
 
+#include <boost/function.hpp>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/stringize.hpp>
 
@@ -32,7 +33,7 @@ static void CODE_TO_TIME(benchmark::State& state)
 BENCHMARK(CODE_TO_TIME);
 
  */
-
+ 
 namespace benchmark {
 
     class State {
@@ -52,7 +53,7 @@ namespace benchmark {
         bool KeepRunning();
     };
 
-    typedef std::function<void(State&)> BenchFunction;
+    typedef boost::function<void(State&)> BenchFunction;
 
     class BenchRunner
     {
