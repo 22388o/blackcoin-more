6c1e0eb687eab265790563a0d7c99c6125ba8c66
Michel van Kessel <michelvankessel@protonmail.com>
2022-02-12 09:47:34 +0100

    change have_config_h to global include

--- src/init.cpp
+++ src/init.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/bitcoin-config.h"
+#include <config/bitcoin-config.h>
 #endif
 
 #include "init.h"
@@ -30,7 +30,6 @@
 #include "script/standard.h"
 #include "script/sigcache.h"
 #include "scheduler.h"
-#include "timedata.h"
 #include "txdb.h"
 #include "txmempool.h"
 #include "torcontrol.h"
